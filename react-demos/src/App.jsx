86e28110c6831eca36d3caea40e884fc3e1aea4d
gideon <kipkerring.gedion@gmail.com>
2025-01-21 17:47:24 +0300

    rendering if else statements

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Person1 from './Components/Person1'
-import Product1 from './Components/Product1'
+import Weather from './Components/Weather'
 
 
 const App = () => {
   return (
     <div>
-      <Person1 name="Gideon" Age={32} />
-      <Product1 name="Iphone" price={3300} />
+      <Weather/> 
     </div>
   )
 }
