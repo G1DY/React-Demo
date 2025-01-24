c4d11a7a6e7c0a7599280a12c320706ae49dff2e
gideon <kipkerring.gedion@gmail.com>
2025-01-24 11:28:46 +0300

    custom hook useId  hook

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,9 +1,13 @@
-import UrlFetch from './CustomHookComponent/UrlFetch'
+import UniqueID from './CustomHookComponent/UniqueID'
 
 
 const App = () => {
   return (
-    <UrlFetch/>
+    <>
+    <UniqueID/>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod quaerat cum repellat quia maiores natus amet, distinctio minus quis placeat dolorum eos quae, perspiciatis provident cupiditate non aut autem atque.</p>
+    <UniqueID/>
+    </>
   )
 }
 
